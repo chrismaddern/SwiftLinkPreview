c1afb06d1c4e2b16802464a774a93e0bbe13fb09
Chris Maddern <chris@chrismaddern.com>
2019-10-25 14:47:40 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,5 +17,11 @@ let package = Package(
         .target(
                 name: "SwiftLinkPreview"
         )
-    ]
+    ],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "SwiftLinkPreview",
+            targets: ["SwiftLinkPreview"]),
+    ],
 )
